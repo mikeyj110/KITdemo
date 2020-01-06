f1daca0e3a203fb058bef6cf56c17de47b50b1ac
mikeyj110 <Mikey_j110@hotmail.com>
2020-01-07 05:59:12 +0900

    update style

--- www/js/app.js
+++ www/js/app.js
@@ -7,7 +7,7 @@ function addTodoPicture() {
         targetWidth : 100,
         targetHeight : 100
     });
-}
+};
 function addTodo(camera_url) {
     var title = $("#todo-title").val();
     var body = $("#todo-body").val();
@@ -18,4 +18,38 @@ function addTodo(camera_url) {
     $.mobile.changePage($("#list-page"));
     $("#todo-list").append("<li>" + img_tag + "<h3>" + title + "</h3><p>" + body + "</p></li>")
     $("#todo-list").listview('refresh');
+};
+
+var help = "help123";
+
+function scanBarcode() {
+    cordova.plugins.barcodeScanner.scan(
+        function(result) {
+            //alert("We got a barcode\n" +
+            //    "Result: " + result.text + "\n" +
+            //    "Format: " + result.format + "\n" +
+            //    "Cancelled: " + result.cancelled);
+            document.getElementById("vin").value = result.text;
+        },
+        function(error) {
+            alert("Scanning failed: " + error);
+        }, {
+            //preferFrontCamera : true, // iOS and Android
+            //showFlipCameraButton : true, // iOS and Android
+            showTorchButton: true, // iOS and Android
+            //torchOn: true, // Android, launch with the torch switched on (if available)
+            saveHistory: true, // Android, save scan history (default false)
+            prompt: "Place a barcode inside the scan area", // Android
+            resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+            //formats: "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+            orientation: "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+            disableAnimations: true, // iOS
+            disableSuccessBeep: false // iOS and Android
+        }
+    );
+};
+function saveResults() {
+  var vin = document.getElementById("vin").value;
+  var assignment = document.getElementById("assignment").value;
+  console.log(vin + " " + assignment)
 };
