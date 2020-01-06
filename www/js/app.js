385f2cb490047d1642d6910903b758a68bb81260
mikeyj110 <Mikey_j110@hotmail.com>
2020-01-07 06:44:14 +0900

    update alerts

--- www/js/app.js
+++ www/js/app.js
@@ -32,24 +32,51 @@ function scanBarcode() {
             document.getElementById("vin").value = result.text;
         },
         function(error) {
-            alert("Scanning failed: " + error);
+            navigator.notification.alert(
+              "Scanning failed: " + error,    // message
+              function(){},                   // callback
+              'Scanning failed',              // title
+              'OK'                            // buttonName
+            );
         }, {
             //preferFrontCamera : true, // iOS and Android
             //showFlipCameraButton : true, // iOS and Android
             showTorchButton: true, // iOS and Android
             //torchOn: true, // Android, launch with the torch switched on (if available)
-            saveHistory: true, // Android, save scan history (default false)
+            //saveHistory: true, // Android, save scan history (default false)
             prompt: "Place a barcode inside the scan area", // Android
             resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
             //formats: "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-            orientation: "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+            //orientation: "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
             disableAnimations: true, // iOS
             disableSuccessBeep: false // iOS and Android
         }
     );
 };
 function saveResults() {
+  var dealer = document.getElementById("dealer").value;
   var vin = document.getElementById("vin").value;
   var assignment = document.getElementById("assignment").value;
-  console.log(vin + " " + assignment)
+  var message = "";
+  if(!dealer || !vin || !assignment){
+    var message = "Please fill out all data";
+    navigator.notification.alert(
+      message,          // message
+      function(){},     // callback
+      'Check Data',  // title
+      'OK'              // buttonName
+    );
+  }
+  else {
+    console.log(vin + " " + assignment);
+  //  alert("VIN " + vin + " has been added to " + dealer + "using code " + assignment);
+    var message = "VIN " + vin + " has been added to " + dealer + "using code " + assignment;
+    var alertCallback = "Success";
+    navigator.notification.alert(
+      message,          // message
+      function(){},     // callback
+      alertCallback,    // title
+      'OK'              // buttonName
+    );
+  }
 };
