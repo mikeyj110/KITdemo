3e7fd1ebd9fba0b882249a54421bbc976498fd19
mikeyj110 <Mikey_j110@hotmail.com>
2020-01-07 11:55:39 +0900

    add restDBreorg page structure

--- www/js/app.js
+++ www/js/app.js
@@ -1,27 +1,3 @@
-function addTodoPicture() {
-    navigator.camera.getPicture(addTodo, function() {
-        alert("Failed to get camera.");
-    }, {
-        quality : 50,
-        destinationType : Camera.DestinationType.FILE_URI,
-        targetWidth : 100,
-        targetHeight : 100
-    });
-};
-function addTodo(camera_url) {
-    var title = $("#todo-title").val();
-    var body = $("#todo-body").val();
-    var img_tag = "";
-    if (camera_url) {
-        img_tag = "<img src='" + camera_url + "'>";
-    }
-    $.mobile.changePage($("#list-page"));
-    $("#todo-list").append("<li>" + img_tag + "<h3>" + title + "</h3><p>" + body + "</p></li>")
-    $("#todo-list").listview('refresh');
-};
-
-var help = "help123";
-
 function scanBarcode() {
     cordova.plugins.barcodeScanner.scan(
         function(result) {
@@ -70,7 +46,7 @@ function saveResults() {
   else {
     console.log(vin + " " + assignment);
   //  alert("VIN " + vin + " has been added to " + dealer + "using code " + assignment);
-    var message = "VIN " + vin + " has been added to " + dealer + "using code " + assignment;
+    var message = "VIN " + vin + " has been added to " + dealer + " using code " + assignment;
     var alertCallback = "Success";
     navigator.notification.alert(
       message,          // message
